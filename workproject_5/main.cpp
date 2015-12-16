1064ef2a9e37fb9e3cd6ff464e1d38737bea7f28
20141105043 <670140530@qq.com>
2015-12-16 18:14:48 +0800

    Initial Commit

---
+++ workproject_5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  workproject_5
+//
+//  Created by 20141105043dj on 15/12/16.
+//  Copyright (c) 2015年 20141105043dj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
