bb049d34a133020b0d9fe406d93dd65f63fb12c3
20141105043 <670140530@qq.com>
2015-12-16 18:51:18 +0800

    inheritance
    定义基类Student数据成员有学号，姓名，性别和继承类Student1数据成员有年龄和地址

--- workproject_5/main.cpp
+++ workproject_5/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    class Student
+    {
+    public:
+        void get_value()
+        {
+            cin>>num>>name>>sex;
+        }
+        void display()
+        {
+            cout<<"num:"<<num<<endl;
+            cout<<"name:"<<name>>endl;
+            cout<<"sex:"<<sex<<endl;
+        }
+    private:
+        int mun;
+        string name;
+        char sex;
+    };
+    class Studentt1:public Student
+    {
+    public:
+        void get_value_1()
+        {
+            cin>>age>>addr;
+        }
+        void display_1()
+        {
+            cout<<"num:"<<num<<endl;
+            cout<<"name:"<<num<<endl;
+            cout<<"sex:"<<num<<endl;
+            cout<<"age:"<<num<<endl;
+            cout<<"address:"<<num<<endl;
+        }
+    private:
+        int age;
+        string addr;
+    }
+}
+int main()
+{
+    Student1 stud;
+    stud.get_value();
+    stud.get_value_1();
+    stud.display();
+    stud.display_1();
     return 0;
+
 }
